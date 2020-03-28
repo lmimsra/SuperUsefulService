febf9af08f1a4c2365595e82e7d16496025e5eab
Lima <lmimsra07@gmail.com>
2020-03-28 19:29:58 +0900

    テーブルにスタイルを当ててみた

--- resources/views/thread.blade.php
+++ resources/views/thread.blade.php
@@ -12,9 +12,12 @@
                         <form method="POST" action="/threads">
                             @csrf
                             <div class="form-group row">
+                                <div class="col-md-2">
+                                    <label for="thread-title" class="col-form-label">タイトル</label>
+                                </div>
                                 <div class="col-md-8">
-                                    <input class="form-control" type="text" name="title" value=""
-                                           placeholder="スレッドタイトル"/>
+                                    <input class="form-control" type="text" name="title" value="" id="thread-title"
+                                           placeholder="新しいスレッドを作ろう！"/>
 
                                     {{--  エラーハンドリング追加したらなんかやるかもしれない  --}}
                                     {{-- @error('email')  --}}
@@ -23,7 +26,7 @@
                                     {{-- </span>  --}}
                                     {{-- @enderror  --}}
                                 </div>
-                                <div class="col-md-4">
+                                <div class="col-md-2">
                                     <button type="submit" class="btn btn-primary">作成</button>
                                 </div>
                             </div>
@@ -32,23 +35,34 @@
                 </div>
             </div>
         </div>
-        <div class="row justify-content-center">
-
+        <div class="row" style="margin-top: 1rem">
             <h1>スレッド一覧</h1>
             {{--  スレッドがあれば表示--}}
-            @if(isset($threads))
-                @foreach ($threads as $thread)
-                    <div>
-                        <a href="{{ url('threads/'.$thread->id) }}">
-                            {{ $thread->title }}
-                            ({{ count($thread->getPostList) }})
-                            {{ $thread->updated_at->format('Y/m/d H:i:s') }}
-                        </a>
-                    </div>
-                @endforeach
-            @else
-                <h2>まだスレッドはありません！</h2>
-            @endif
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>スレッド名</td>
+                    <td>コメント数</td>
+                    <td>最終更新</td>
+                </tr>
+                </thead>
+                @if(isset($threads))
+                    <tbody>
+                    @foreach ($threads as $thread)
+                        <tr>
+                            <td>{{$thread->id}}</td>
+                            <td><a style="display: block; width: 100%; height: 100%;"
+                                   href="{{ url('threads/'.$thread->id) }}">{{ $thread->title }}</a></td>
+                            <td>{{ count($thread->getPostList) }}</td>
+                            <td>{{ $thread->updated_at->format('Y/m/d H:i:s') }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                @else
+                    <h2>まだスレッドはありません！</h2>
+                @endif
+            </table>
         </div>
     </div>
 @endsection
