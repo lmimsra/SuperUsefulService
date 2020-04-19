f41e5f5b45a5264ab102a43be88d98c32c35abd9
Lima <lmimsra07@gmail.com>
2020-04-20 00:06:39 +0900

    トップへリンク追加。postページスタイルちょっとかっこよく変更

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,34 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/threads">スレッド一覧に戻る</a>
-    <h1>{{$thread->title}}</h1>
+    <div class="container">
 
-    {{-- 新規投稿 --}}
-    <form method="post" action="/posts">
-        {{ csrf_field() }}
-        <input type="hidden" name="thread_id" value="{{$thread->id}}"/>
-        <p>
-            名前：<label>
-                <input type="text" name="name" value=""/>
-            </label>
-            <input type="submit" value="投稿"/>
-        </p>
-        <p>
-            内容：<textarea name="content" rows="4" cols="40"></textarea>
-        </p>
-    </form>
+        <a href="/threads">スレッド一覧に戻る</a>
+        <h1>{{$thread->title}}</h1>
 
-    {{-- 投稿一覧 --}}
-    @if(isset($thread))
-    @foreach ($thread->getPostList as $post)
-        {{ $post->created_at->format('Y/m/d H:i:s') }}（{{ $post->name }}）
-        <div>
-            {{-- HTMLタグ無効化してから、改行を<br>変換  --}}
-            {!! nl2br(htmlspecialchars($post->content)) !!}
-        </div>
-    @endforeach
-    @else
-        <h3>コメントはまだありません！</h3>
-    @endif
+        {{-- 新規投稿 --}}
+        <form method="post" action="/posts">
+            {{ csrf_field() }}
+            <input type="hidden" name="thread_id" value="{{$thread->id}}"/>
+            <p>
+                名前：<label>
+                    <input type="text" name="name" value=""/>
+                </label>
+                <input type="submit" value="投稿"/>
+            </p>
+            <p>
+                内容：<textarea name="content" rows="4" cols="40"></textarea>
+            </p>
+        </form>
+
+        {{-- 投稿一覧 --}}
+        @if(isset($thread))
+            @foreach ($thread->getPostList as $post)
+                {{ $post->created_at->format('Y/m/d H:i:s') }}（{{ $post->name }}）
+                <div>
+                    {{-- HTMLタグ無効化してから、改行を<br>変換  --}}
+                    {!! nl2br(htmlspecialchars($post->content)) !!}
+                </div>
+            @endforeach
+        @else
+            <h3>コメントはまだありません！</h3>
+        @endif
+    </div>
 @endsection
